7c6b858a2c39dd21f97a1d6e5af50d8781a50d18
Rolena Williams <rolenawilliams@Rolenas-MacBook-Air.local>
2026-03-23 08:17:47 -0400

    Added dark mode to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,30 +3,30 @@ import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart';
 import 'data/preference_helper.dart'; //imports for screens
 
-void main() { //need async code before launching app
-  runApp(FocusStudioApp()); //launch app with saved theme setting
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); //for shared preferences
+  final bool isDarkMode = await PreferencesHelper.getDarkMode(); //loads saved theme before app starts
+  runApp(FocusStudioApp(isDarkMode: isDarkMode));
 }
 
 class FocusStudioApp extends StatelessWidget { //root widget
+  final bool isDarkMode;
+  const FocusStudioApp({super.key,required this.isDarkMode,});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Adaptive Focus Studio', //title
       debugShowCheckedModeBanner: false, //remove debug label
+      themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light, //choose light or dark mode
       theme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: Colors.indigo,
-        scaffoldBackgroundColor: const Color(0xFFF7F8FC),
-        appBarTheme: const AppBarTheme( //keeps app clean looking
-          centerTitle: true,
-          elevation: 0,
-        ),
-        inputDecorationTheme: InputDecorationTheme( //ensuring text style is consistent
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-        ),
+      ),
+        darkTheme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.indigo,
+        brightness: Brightness.dark,
       ),
       home: const WelcomeScreen(),
     );
